1c27a428420239ce6815633d7083adbf8779b7bd
minhaz082 <minhaz082@hotmail.com>
2015-05-07 12:10:22 +0600

    Any file upload code Uploaded

---
+++ FileUploadTest.aspx
@@ -0,0 +1,57 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MegnaSolar.Master" AutoEventWireup="true"
+    EnableEventValidation="false" CodeBehind="FileUploadTest.aspx.cs" Inherits="EATL.WebClient.CommonUI.FileUploadTest" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="HeadContent" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <h1>Save and Download Files from file system</h1>
+    <br />
+    <div>
+        <asp:Label ID="lblwarning" runat="server"></asp:Label><br />
+        <asp:FileUpload ID="fileUpload1" runat="server" /><br />
+        <asp:Button ID="btnUpload" runat="server" Text="Upload" OnClick="btnUpload_Click" /><br />
+    </div>
+    <div>
+        <asp:GridView ID="gvDetails" CssClass="Gridview" runat="server" AutoGenerateColumns="false"
+             AllowPaging="False" OnRowCancelingEdit="gvDetails_RowCancelingEdit" DataKeyNames="IID" CellPadding="4"
+                OnRowEditing="gvDetails_RowEditing" OnRowUpdating="gvDetails_RowUpdating">
+                <RowStyle HorizontalAlign="Center" />
+                <AlternatingRowStyle HorizontalAlign="Center" />   
+            <Columns>                
+                <asp:TemplateField HeaderText="Sl." HeaderStyle-Width="30px">
+                        <ItemTemplate>
+                            <asp:Label ID="lblSRNO" runat="server" Text='<%#Container.DataItemIndex+1 %>'></asp:Label>
+                        </ItemTemplate>
+                </asp:TemplateField>               
+                <asp:TemplateField HeaderText="FileName" HeaderStyle-Width="300px">
+                    <ItemTemplate>
+                        <asp:Label ID="lblFilenAME" runat="server" Text='<%# Eval("FileName") %>'></asp:Label>
+                    </ItemTemplate>
+                </asp:TemplateField>
+                <asp:TemplateField HeaderText="FileLocation" HeaderStyle-Width="300px">
+                        <ItemTemplate>
+                            <asp:Label ID="lblFilePath" runat="server" Text='<%# Eval("FilePath") %>'></asp:Label>
+                        </ItemTemplate>
+                        <EditItemTemplate>                            
+                            <asp:Label ID="lblFilePath" runat="server" Text='<%# Eval("FilePath") %>'></asp:Label>
+                            <asp:FileUpload ID="FileUploadMod" runat="server" />
+                        </EditItemTemplate>
+                </asp:TemplateField>
+                <asp:TemplateField HeaderText="View" HeaderStyle-Width="100px" >
+                    <ItemTemplate>
+                        <asp:LinkButton ID="lnkDownload" runat="server" Text="Download" OnClick="lnkDownload_Click"></asp:LinkButton>
+                    </ItemTemplate>
+                </asp:TemplateField>
+                <asp:TemplateField HeaderStyle-Width="100px" HeaderText="Change">
+                        <ItemTemplate>
+                            <asp:LinkButton ID="LkB1" runat="server" CommandName="Edit">Modify</asp:LinkButton>
+                        </ItemTemplate>
+                        <EditItemTemplate>
+                            <asp:LinkButton ID="LkB2" runat="server" CommandName="Update">Update</asp:LinkButton>
+                            <asp:LinkButton ID="LkB3" runat="server" CommandName="Cancel">Cancel</asp:LinkButton>
+                        </EditItemTemplate>
+                    </asp:TemplateField>
+            </Columns>
+        </asp:GridView>
+    </div>    
+</asp:Content>
---
+++ FileUploadTest.aspx.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using System.IO;
+using EATL.BLL.Facade;
+
+namespace EATL.WebClient.CommonUI
+{
+    public partial class FileUploadTest : System.Web.UI.Page
+    {
+        private SqlConnection con = new SqlConnection(@"Data Source=192.168.1.4;Initial Catalog=MeghnaSolarTestB;User ID=sa;Password=dba@123");
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                BindGridviewData();
+            }
+        }
+        
+        private void BindGridviewData()
+        {
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter("Select IID,FileName,FilePath,CreateBy,CreateDate,UpdateBy,Updatedate,IsRemove from FileUploadForTest", con);
+            con.Open();
+            da.Fill(dt);
+            con.Close();
+
+            if (dt.Rows.Count > 0)
+            {
+                gvDetails.DataSource = dt;
+                gvDetails.DataBind();
+            }
+        }
+        // Save files to Folder and files path in database
+        protected void btnUpload_Click(object sender, EventArgs e)
+        {
+            if (fileUpload1.PostedFile.FileName != "")
+            {
+                lblwarning.Text = "";
+                string filename = Path.GetFileName(fileUpload1.PostedFile.FileName);
+                fileUpload1.SaveAs(Server.MapPath("Files/" + filename));
+                con.Open();
+                SqlCommand cmd = new SqlCommand("insert into FileUploadForTest(FileName,FilePath,CreateBy,CreateDate,UpdateBy,Updatedate,IsRemove) values(@FileName,@FilePath,@CreateBy,@CreateDate,@UpdateBy,@Updatedate,@IsRemove)", con);
+                cmd.Parameters.AddWithValue("@FileName", filename);
+                cmd.Parameters.AddWithValue("@FilePath", "Files/" + filename);
+                cmd.Parameters.AddWithValue("@CreateBy", Convert.ToInt64(Session["UserID"]));
+                cmd.Parameters.AddWithValue("@CreateDate", GeneralConfigurationFacade.GetServerDateTime());
+                cmd.Parameters.AddWithValue("@UpdateBy", Convert.ToInt64(Session["UserID"]));
+                cmd.Parameters.AddWithValue("@Updatedate", GeneralConfigurationFacade.GetServerDateTime());
+                cmd.Parameters.AddWithValue("@IsRemove", "False");
+
+                cmd.ExecuteNonQuery();
+                con.Close();
+                BindGridviewData();
+            }
+        }
+        // This button click event is used to download files from gridview
+        protected void lnkDownload_Click(object sender, EventArgs e)
+        {
+            LinkButton lnkbtn = sender as LinkButton;
+            GridViewRow gvrow = lnkbtn.NamingContainer as GridViewRow;           
+            Label Path = (Label)gvDetails.Rows[gvrow.RowIndex].FindControl("lblFilePath");
+            string filePath = Path.Text;
+            Response.ContentType = "image/jpg";
+            Response.AddHeader("Content-Disposition", "attachment;filename=\"" + filePath + "\"");
+            Response.TransmitFile(Server.MapPath(filePath));            
+            Response.End();
+            lblwarning.Text = "";
+        }        
+        //edit event
+        protected void gvDetails_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            gvDetails.EditIndex = e.NewEditIndex;
+            BindGridviewData();
+            lblwarning.Text = "";
+        }
+        // update event
+        protected void gvDetails_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            string FileId;
+            string FileName;
+            string FilePath;
+            FileId = gvDetails.DataKeys[e.RowIndex].Value.ToString();
+            FileUpload FileUploadMod = (FileUpload)gvDetails.Rows[e.RowIndex].FindControl("FileUploadMod");           
+            //FileName = FileUploadMod.PostedFile.FileName;
+            //FilePath = "Files/" + FileUploadMod.PostedFile.FileName;
+            if (FileUploadMod.HasFile)
+            {
+                lblwarning.Text = "";
+                FileName = FileUploadMod.PostedFile.FileName;
+                FilePath = "Files/" + FileUploadMod.PostedFile.FileName;
+                FileUploadMod.SaveAs(Server.MapPath("Files/" + FileUploadMod.FileName));
+
+            }
+            else
+            {
+                Label Path = (Label)gvDetails.Rows[e.RowIndex].FindControl("lblFilePath");
+                FilePath = Path.Text;
+                Label Name = (Label)gvDetails.Rows[e.RowIndex].FindControl("lblFilenAME");
+                FileName = Name.Text;
+            }
+
+            SqlCommand cmd = new SqlCommand("update FileUploadForTest set FileName='" + FileName + "', FilePath='" + FilePath + "' where IID=" + FileId + "", con);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+            gvDetails.EditIndex = -1;
+            BindGridviewData();
+
+        }
+        // cancel edit event
+        protected void gvDetails_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            gvDetails.EditIndex = -1;
+            BindGridviewData();
+            lblwarning.Text = "";
+        }
+    }
+}
